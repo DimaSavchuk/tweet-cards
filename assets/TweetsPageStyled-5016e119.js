9a6c6df82d2adfc3f1ac459eca4bd1c30e1c3aba
DimaSavchuk <dima.savchuk.011@gmail.com>
2023-10-15 16:11:24 +0000

    Deploying to gh-pages from @ DimaSavchuk/tweet-cards@b5f093319c0b280236a47ca367742802d5ee6134 🚀

---
+++ assets/TweetsPageStyled-5016e119.js
@@ -0,0 +1,58 @@
+import{s as t,N as o}from"./index-2c4ecca9.js";const e=t.div`
+  padding-top: 20px;
+  display: flex;
+  gap: 30px;
+`,a=t(o)`
+  background: #ebd8ff;
+  box-shadow: 0px 3.43693px 3.43693px 0px rgba(0, 0, 0, 0.25);
+
+  color: black;
+  padding: 14px 28px;
+  border-radius: 10.3px;
+
+  border: none;
+
+  font-family: inherit;
+  font-size: 18px;
+  font-weight: 600;
+  text-transform: uppercase;
+
+  margin-bottom: 8px;
+`,n=t.select`
+  background: #ebd8ff;
+  box-shadow: 0px 3.43693px 3.43693px 0px rgba(0, 0, 0, 0.25);
+
+  width: 192px;
+  padding: 14px 28px;
+  border-radius: 10.3px;
+
+  border: none;
+
+  font-family: inherit;
+  font-size: 18px;
+  font-weight: 600;
+  text-transform: uppercase;
+
+  margin-bottom: 8px;
+`,r=t.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 48px;
+`,x=t.button`
+  background: #ebd8ff;
+  box-shadow: 0px 3.43693px 3.43693px 0px rgba(0, 0, 0, 0.25);
+
+  width: 192px;
+  padding: 14px 28px;
+  border-radius: 10.3px;
+
+  border: none;
+
+  font-family: inherit;
+  font-size: 18px;
+  font-weight: 600;
+  text-transform: uppercase;
+
+  margin-bottom: 8px;
+`;export{e as C,x as S,r as T,a,n as b};
