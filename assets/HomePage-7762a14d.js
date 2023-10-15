227156071ad0a52fd76beeb0e5cf6a139ba0e927
DimaSavchuk <dima.savchuk.011@gmail.com>
2023-10-15 17:48:34 +0000

    Deploying to gh-pages from @ DimaSavchuk/tweet-cards@fa5bdaa5123e50dd76c02d3b6fb0ba964bbe0758 🚀

--- assets/HomePage-7762a14d.js
+++
@@ -1,8 +0,0 @@
-import{s as t,j as e,L as s,R as i}from"./index-2c4ecca9.js";import{S as n}from"./TweetsPageStyled-5016e119.js";const o=t.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  height: 90vh;
-  width: 90vw;
-`,a=()=>e.jsxs(o,{children:[e.jsx("h1",{style:{color:"white"},children:"Home page"}),e.jsx(s,{to:i.TWEETS,children:e.jsx(n,{children:"To Tweets"})})]});export{a as default};
